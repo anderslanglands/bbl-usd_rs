2005610ebfffaa1893abd09fd695e80b0756a01a
Anders Langlands <anderslanglands@gmail.com>
2022-10-15 17:08:57 +1300

    some cleanup and comments

--- src/gen.rs
+++ src/gen.rs
@@ -7,8 +7,6 @@ pub fn main() -> Result<()> {
         .format_timestamp(None)
         .init();
 
-    // Point CMake to our library. In a real project we would probably expect this to be done by setting CMAKE_PREFIX_PATH
-    // directly in the environment, or perhaps with a config file
     let cmake_prefix_path = PathBuf::from(env!("CMAKE_PREFIX_PATH"));
 
     let namespace_internal = "pxrInternal_v0_22__pxrReserved__";
@@ -18,17 +16,17 @@ pub fn main() -> Result<()> {
     let mut override_list = vec![];
     let mut binding_includes = vec![];
 
-    let mut binding_fns = Vec::new();
+    // each of these function calls populates the allow list and includes list and returns a closure we'll call after
+    // the AST is extracted to do any modifications needed
+    let binding_fns = vec![
+        bind_vtvalue(&mut allow_list, &mut binding_includes),
+        bind_sdf_path(&mut allow_list, &mut binding_includes),
+        bind_usd_property(&mut allow_list, &mut binding_includes),
+        bind_usd_prim(&mut allow_list, &mut binding_includes, &mut override_list),
+    ];
 
-    binding_fns.push(bind_vtvalue(&mut allow_list, &mut binding_includes));
-    binding_fns.push(bind_sdf_path(&mut allow_list, &mut binding_includes));
-    binding_fns.push(bind_usd_property(&mut allow_list, &mut binding_includes));
-    binding_fns.push(bind_usd_prim(
-        &mut allow_list,
-        &mut binding_includes,
-        &mut override_list,
-    ));
-
+    // replace the external namespace name with the internal one in the regexes so we can write "pxr..." instead of the
+    // ridiculous versioned one pixar use.
     let allow_list: Vec<String> = allow_list
         .iter()
         .map(|s| s.replace(namespace_external, namespace_internal))
@@ -44,13 +42,23 @@ pub fn main() -> Result<()> {
         // to find the target cpp library as well as provide the library name for find_package() and the actual targets
         // to link against
         cmake_prefix_path: Some(cmake_prefix_path),
+        // These get translated directly to find_packages() calls in CMake
         find_packages: &["pxr REQUIRED"],
+        // These get stuck directly into the target_link_libraries() call for the shim
         link_libraries: &["vt"],
         // We can limit our extraction to a single namespace in the target library. This is usually a good idea to
         // avoid doing extra work (bbl-extract will extract everything it finds, even if it's never used, and the less
         // c++ it has to exract, the less likely it is to choke on constructs we haven't implemented yet)
+        // This will be replaced at some point soon with the AllowList mechanism
         limit_to_namespace: Some(namespace_internal),
+        // AllowList is just a list of qualified name regexes for things that we want to extract. See the binding functions
+        // for examples. We might want to augment this with separate allow/block-lists per decl kind (e.g. classes,
+        // methods etc)
         allow_list: AllowList::new(allow_list),
+        // OverrideList is a list of pairs of regexes and callbacks that allow to supply a manually generated AST for a
+        // type. See create_tfweakptr() below for an example. Ideally we wouldn't need this as everything could be
+        // extracted, but we need a stopgap for when there's particularly painful constructs like TfWeakPtr's use of
+        // CRTP.
         overrides: OverrideList::new(override_list),
         // compile_definitions: &["-Wno-deprecated"],
         ..Default::default()
@@ -65,6 +73,7 @@ pub fn main() -> Result<()> {
     let ns = ast.find_namespace(namespace_internal)?;
     ast.rename_namespace(ns, namespace_external);
 
+    // debug-printing the AST dumps everything for inspection
     println!("{ast:?}");
 
     // now call all the binding functions to manipulate the ast
@@ -93,30 +102,16 @@ fn bind_usd_property(
 ) -> Box<dyn Fn(&mut AST) -> Result<()>> {
     includes.push("#include <pxr/usd/usd/property.h>");
 
+    // Note we have separate regexes for UsdProperty and UsdProperty::*. This is to avoid binding any functions that
+    // begin with UsdProperty, as there tends to be a lot of these that are templated which we want to ignore anyway.
+    //
+    // As a general rule, while babble is being developed, a good strategy is to try binding a type like UsdProperty is
+    // done, below. If the shim compiles, then all good you can carry on to the next one. If it fails, then figure out
+    // what construct is causing it to fail and make a test case reproducing it in either bbl-extact, bbl-translate,
+    // bbl-write or some combination of those, then fix the issue.
     allow_list.extend_from_slice(&[
         r"^pxr::UsdProperty$",
         r"^pxr::UsdProperty::.*$",
-        // r"^pxr::UsdProperty::UsdProperty\(.*\)$",
-        // r"^pxr::UsdProperty::GetPropertyStack\(.*\)$",
-        // r"^pxr::UsdProperty::GetBaseName\(.*\)$",
-        // r"^pxr::UsdProperty::GetNamespace\(.*\)$",
-        // r"^pxr::UsdProperty::SplitName\(.*\)$",
-        // r"^pxr::UsdProperty::GetDisplayGroup\(.*\)$",
-        // r"^pxr::UsdProperty::SetDisplayGroup\(.*\)$",
-        // r"^pxr::UsdProperty::ClearDisplayGroup\(.*\)$",
-        // r"^pxr::UsdProperty::HasAuthoredDisplayGroup\(.*\)$",
-        // r"^pxr::UsdProperty::GetNestedDisplayGroups\(.*\)$",
-        // r"^pxr::UsdProperty::SetNestedDisplayGroups\(.*\)$",
-        // r"^pxr::UsdProperty::GetDisplayName\(.*\)$",
-        // r"^pxr::UsdProperty::SetDisplayName\(.*\)$",
-        // r"^pxr::UsdProperty::ClearDisplayName\(.*\)$",
-        // r"^pxr::UsdProperty::HasAuthoredDisplayName\(.*\)$",
-        // r"^pxr::UsdProperty::IsCustom\(.*\)$",
-        // r"^pxr::UsdProperty::SetCustom\(.*\)$",
-        // r"^pxr::UsdProperty::IsDefined\(.*\)$",
-        // r"^pxr::UsdProperty::IsAuthored\(.*\)$",
-        // r"^pxr::UsdProperty::IsAuthoredAt\(.*\)$",           // UsdEditTarget
-        // r"^pxr::UsdProperty::FlattenTo\(.*\)$",
         r"^pxr::SdfHandle<T>::operator->.*$",
         r"^pxr::SdfPropertySpec.*Handle.*$",
     ]);
@@ -144,6 +139,8 @@ fn bind_usd_prim(
     includes: &mut Vec<&str>,
     overrides: &mut Vec<(&str, Box<ClassExtractionFn>)>,
 ) -> Box<dyn Fn(&mut AST) -> Result<()>> {
+    // we need to add all these includes here as prim.h contains fwd decls for all this stuff so we need the actual
+    // includes for the shim.
     includes.push("#include <pxr/usd/usd/property.h>");
     includes.push("#include <pxr/usd/usd/prim.h>");
     includes.push("#include <pxr/usd/usd/attribute.h>");
@@ -163,6 +160,7 @@ fn bind_usd_prim(
         r"^pxr::UsdPrim::.*$",
     ]);
 
+    // We manually bind TfWeakPtr because it uses CRTP which we haven't figured out a way to extract cleanly yet
     overrides.push((
         r"^pxr::TfWeakPtr<.*>$",
         Box::new(
@@ -235,6 +233,9 @@ fn bind_vtvalue(
     })
 }
 
+/// This function takes a class id, a list of signiatures and a list of types, and for each signature and type combination
+/// generates a template specialization for that method.
+/// We can use this to quickly specialize e.g. VtValue for all the different types we want
 pub fn specialize_methods(
     ast: &mut AST,
     class_id: ClassId,
@@ -272,14 +273,15 @@ pub fn create_tfweakptr(
         already_visited.push(c.usr());
     }
 
-    println!("Got TfWeakPtr {c:?}");
-
+    // first create the template class which will contain the actual methods
     let c_tmpl = c.specialized_template().unwrap();
     let usr_tmpl = create_tfweakptr_tmpl(c_tmpl, ast, tu, already_visited)?;
 
     let name = c.display_name();
 
     let namespaces = get_namespaces_for_decl(c, tu, ast, already_visited)?;
+
+    // Get the first template argument for the decl we're extracting
     let ty = c.template_argument_type(0)?;
     let template_arguments = vec![TemplateArgument::Type(extract_type(
         ty,
@@ -291,9 +293,13 @@ pub fn create_tfweakptr(
         class_overrides,
     )?)];
 
+    // create a new specialization of our manually generate template class with the template argument type we've extracted
     let cts =
         ClassTemplateSpecialization::new(usr_tmpl, c.usr(), &name, template_arguments, namespaces);
 
+    // and insert it into the AST and add the specialization to the template class. The latter step just silences a
+    // warning about it being ignored currently, but later we'll need this info in order to generate nice names for the
+    // specialized classes by associating them with typedefs
     let id = ast.insert_class_template_specialization(cts);
     let cd = ast.get_class_mut(usr_tmpl).unwrap();
     cd.add_specialization(id);
@@ -313,16 +319,12 @@ fn create_tfweakptr_tmpl(
         already_visited.push(c_tmpl.usr());
     }
 
-    // get the namespaces for std::vector<> as we might not have found them already
+    // construct the full namespace path for the methods including the class template
     let namespaces = get_namespaces_for_decl(c_tmpl, tu, ast, already_visited)?;
+    let mut method_namespaces = namespaces.clone();
+    method_namespaces.push(c_tmpl.usr());
 
-    let u_std = ast
-        .find_namespace("std")
-        .map(|id| ast.namespaces()[id].usr())
-        .unwrap();
-
-    let method_namespaces = vec![u_std, c_tmpl.usr()];
-
+    // manually create the AST for the methods we care about
     let methods = vec![
         Method::new(
             USR::new("BBL:tfweakptr_ctor_default"),
@@ -374,12 +376,13 @@ fn create_tfweakptr_tmpl(
         ),
     ];
 
+    // and finally create the ClassDecl itself and add it to the AST
     let cd = ClassDecl::new(
         c_tmpl.usr(),
         "TfWeakPtr".to_string(),
         Vec::new(),
         methods,
-        vec![u_std],
+        namespaces,
         vec![TemplateParameterDecl::typ("T", 0)],
         false,
         NeedsImplicit {
